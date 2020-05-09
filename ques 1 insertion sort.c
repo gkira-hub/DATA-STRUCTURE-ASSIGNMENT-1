b409664f8def3bdd99cda2a5c0d70c372546d3d9
gkira-hub <54792309+gkira-hub@users.noreply.github.com>
2020-05-09 14:48:29 +0530

    Update and rename i)_a)_insertion sort.c to ques 1 insertion sort.c

--- ques 1 insertion sort.c
+++ ques 1 insertion sort.c
@@ -1,3 +1,4 @@
+//----------------INSERTIOM SORT------------------------
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,26 +7,23 @@ int main()
     int n,temp,x,y;
     printf("enter size of array:");
     scanf("%d",&n);
-    int a[2*n];
+    int a[n];
     printf("enter elements of array:");
-   for(int i=0;i<2*n;i++)
-        a[i]=77777777;
-    printf("enter elements of array:");
-    for(int i=n-1;i<2*n-1;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     printf("unsorted array:");
-    for(int i=n-1;i<2*n-1;i++)
+    for(int i=0;i<n;i++)
     {
         printf("%d\t",a[i]);
     }
-    for( x=n-1;x<2*n-1;x++)
+    for( x=1;x<n;x++)
     {
         temp=a[x];
-        for(y=x-1;y>=0 && a[y]>temp;y--)
+        for(y=x-1;y>=0 && a[y]>temp;y--)//comparison
         {
-            a[y+1]=a[y];
+            a[y+1]=a[y];//movement
         }
         a[y+1]=temp;
     }
